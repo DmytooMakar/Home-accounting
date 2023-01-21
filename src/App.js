9637279b7eac8435b39c442b6312146e615fe773
Dmytro Makar <mishamakar5@gmail.com>
2023-01-21 13:59:45 +0200

    add packs and started create router elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './components/HomePage/Home';
+import Accounting from './components/WorkingPage/Accounting';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+    <h1>Hello</h1>
+    <Router>
+      <header>
+        <nav>
+          <ul className='navigation'>
+            <li >
+              <Link className='link' to="/">Home</Link>
+            </li>
+            <li>
+              <Link className='link' to="/logIn">Log In</Link>
+            </li>
+            <li >
+              <Link className='link' to="/accounting">System</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/logIn">
+
+          </Route>
+          <Route path="/accounting">
+            <Accounting />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </header>   
+    </Router>
+  </>
+    
   );
 }
 
---
+++ src/components/WorkingPage/Accounting.jsx
@@ -0,0 +1,5 @@
+export default function Accounting() {
+    return (
+        <h1>Hello world Page 2</h1>
+    )
+}

12ca8e3684066c56de7250c5bf88b4c3840fca0a
Dmytro <mishamakar5@gmail.com>
2023-02-02 18:17:46 +0200

    add function push autorization push

--- src/components/Pages/Authorization/Authorization.jsx
+++ src/components/Pages/Authorization/Authorization.jsx
@@ -1,3 +1,4 @@
+import axios from "axios"
 import {
     Form,
     Button
@@ -6,13 +7,37 @@ import {
 import "./Authorization.css"
 
 
-export default function Authorization() {
+export default function Authorization({user, setUser}) { 
+
+    function sendRegistrationForm(){
+        axios.post(`http://localhost:3001/users`, {
+            email: user.email,
+            password: user.password
+        })
+    }
+
+    function onChangeEmail(target){
+        setUser((prev) => ({ 
+            ...prev,
+            email: target.target.value
+        }))
+    }
+
+    function onChangePassword(target){
+        setUser((prev) => ({ 
+            ...prev,
+            password: target.target.value
+        }))
+    }
+
+    console.log(user)
+
     return (
     <main className="form-main-authorization">
         <Form>
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" value={user.email} placeholder="Enter email" onChange={onChangeEmail}/>
                 <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
                 </Form.Text>
@@ -20,12 +45,9 @@ export default function Authorization() {
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" value={user.password} placeholder="Password" onChange={onChangePassword}/>
             </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" onClick={sendRegistrationForm}>
                 Submit
             </Button>
         </Form>
